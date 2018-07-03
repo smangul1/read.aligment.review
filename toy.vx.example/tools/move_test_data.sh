19b2764286940b8cb1c180d3e445ef22151e0e4d
Victor Xue <victorxue007@gmail.com>
2018-07-02 23:59:41 -0700

    Tested cushaw3, added test data, added scripts to create test data

--- toy.vx.example/tools/move_test_data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Argument 1 is the file name to copy the fastq and fasta files to
-cp ~/read.aligment.review/toy.vx.example/toy.test.data/toy.long_reads.fq $1/toy.long.reads.fq
-cp ~/read.aligment.review/toy.vx.example/toy.test.data/toy.short_reads.fq $1/toy.short.reads.fq
-cp ~/read.aligment.review/toy.vx.example/toy.test.data/toy.reference.fa $1/toy.reference.fa
-
-
